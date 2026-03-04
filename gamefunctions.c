b8c592d572529a8006bda3a26fe63fa06635813e
evo-vii <sanicolmg@gmail.com>
2026-03-04 15:16:36 +0800

    Finished playerInit() Ver.1

--- gamefunctions.c
+++ gamefunctions.c
@@ -7,9 +7,28 @@ GAME PROPER FUNCTIONS FILE.
 #include "defs.h"
 
 
-int playerInit()
+// SUPLEMENTARY GAME FUNCTIONS
+
+// DEVNOTE-Lance: This is widely experimental and not yet to be implemented.
+void displayDeck()
+{
+
+}
+
+// PRIMARY GAME FUNCTIONS
+
+/*
+    The function playerInit() is the first stage of the game proper. (TBC)
+
+    // DEVNOTE-Lance: Players are hard-coded in this version of the game function, adding new players, usernames and file handling will be added in the future.
+*/
+int playerInit(int nPlyrCnt)
 {
     int nCount = 0;
+    int nCtr;
+
+    // Inserted temporary usernames while file handling is not yet set.
+    Username plyrUN[6] = {"Antioch", "Constantinople", "Ur", "Northumbria", "Gaul", "Prussia"};
     
     do
     {
@@ -17,22 +36,36 @@ int playerInit()
         scanf("%d", &nCount);
     } while (nCount > 6 || nCount < 3);
 
+    for (nCtr = 0; nCtr < nCount; nCtr++)
+    {
+        printf("P%d: %s\n", nCtr+1, plyrUN[nCtr]);
+    }
 
-
-    return 999;
+    return nCount;
 }
 
+/*
+    The function deckInit()...
+*/
 void deckInit(int nPlyrCnt)
 {
 
+
+
 }
 
+/*
+    The function scoreFlow()...
+*/
 int scoreFlow()
 {
 
     return 999;
 }
 
+/*
+    The function stealFlow()...
+*/
 int stealFlow()
 {
 
@@ -40,6 +73,9 @@ int stealFlow()
     return 999;
 }
 
+/*
+    The function runGame()...
+*/
 void runGame()
 {
 
@@ -48,8 +84,15 @@ void runGame()
 
 int main()
 {
+    
+    // Player Count Variable
+    int nPlyrCnt;
 
-    playerInit();
+    // Pre-game Preparations
+    nCount = playerInit(&nPlyrCnt);
+    deckInit(nPlyrCnt);
+
+    // Actual Game Run
 
 
     return 0;
