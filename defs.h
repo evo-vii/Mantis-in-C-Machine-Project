72bb5871ae1b008b254d362fee1b7dad9e72670c
evo-vii <sanicolmg@gmail.com>
2026-03-29 19:47:49 +0800

    Modified Comments

--- defs.h
+++ defs.h
@@ -25,40 +25,52 @@
 #define MAX_PLAYER_LOAD 50
 
 // Type Definitions
-typedef char Username[USERCHAR_MAX];
+typedef char Username[USERCHAR_MAX]; // Type Definition for storing player names
 
+/**
+    Holds all details regarding a single card in Mantis.
+*/
 typedef struct CardDetails
 {
-    char cFront;
-    char cBack[3];
-    int nPointVal;
+    char cFront;   // Front face of card
+    char cBack[3]; // Back colors of a card
+    int nPointVal; // Attributed point value of card
 } Card;
 
+/**
+    Central structure holding all primary details to be tracked in the game.
+*/
 typedef struct GameBase
 {
-    int nPlyrCtr;
-    Card deck[MAX_CARDS];
+    int nPlyrCtr;         // Total number of game players
+    Card deck[MAX_CARDS]; // The game deck
 } GameState;
 
+/**
+    The player's deck, and relevant deck information.
+*/
 typedef struct PDeck
 {
-    int nTankAmt; // Tank Array Indexer
-    int nTankCtr; // Actual Number of Tank Cards
-    int nColorCards[COLORS];
-    Card tankPile[MAX_CARDS];
+    int nTankAmt;              // Max used index of tank pile
+    int nTankCtr;              // Actual Number of Tank Cards
+    int nColorCards[COLORS];   // Tracker for tank pile colors
+    Card tankPile[MAX_CARDS];  // Tank pile
 
-    int nScoreAmt;
-    int nCurrScore;
-    Card scorePile[MAX_CARDS];
+    int nScoreAmt;             // Max used index of score pile
+    int nCurrScore;            // Current Score of player
+    Card scorePile[MAX_CARDS]; // Score pile
 } PlayerDeck;
 
+/**
+    All relevant variables regarding the player. Includes data imported from players.txt
+*/
 typedef struct PDT
 {
-    Username playerName;
-    int nPNum;
-    int nScoreMax;
-    int nGameWins;
-    PlayerDeck theDeck;
+    Username playerName; // Player username
+    int nPNum;           // Player number during gameplay
+    int nScoreMax;       // Player's all-time high score
+    int nGameWins;       // Player's all time game wins
+    PlayerDeck theDeck;  // Player's deck structure
 
 } PlayerData;
 
